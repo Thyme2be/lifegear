1e2d196d095c1e8fbd2cd283759943e1df15de7c
Trin Chuainugoon <trinchuaynugoon@gmail.com>
2025-08-21 19:34:28 +0700

    Structure the file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,31 @@
-from typing import Union
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from supabase import create_client, Client
+from utils.security import hash_password, verify_password
 
-from fastapi import FastAPI
+# Load .env file
+load_dotenv()
 
 app = FastAPI()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -10,6 +33,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class studentLogin(BaseModel):
+    studentId: int
+    password: str
+
+
+@app.post("/login")
+def login(req: studentLogin):
+    pass
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,58 @@
-const page = () => {
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    console.log("Form data being sent:", formData);
+
+    try {
+      const res = await axios.post("http://localhost:8000/login", formData);
+      console.log("Response from backend:", res.data);
+      alert("Login successful");
+    } catch (error) {
+      console.error("ERROR WHILE LOGIN", error);
+    }
+  }
+
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <main className=" text-3xl flex justify-center items-center h-screen ">
+      <form
+        onSubmit={handleLogin}
+        className=" flex flex-col gap-5 w-fit h-fit p-10 border "
+      >
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className=" border "
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className=" border "
+        />
+        <button type="submit" className=" cursor-pointer bg-blue-600 ">
+          Login
+        </button>
+      </form>
+    </main>
   );
 };
 
-export default page;
+export default Page;
