9432f83fc57acf055cb24a50163b53bc33c1d111
merr1412 <khlueabwichit@gmail.com>
2025-11-01 17:57:41 +0700

    Dev -- Create Monthly Route Classes

--- backend/app/api/v1/routes/calendar.py
+++ backend/app/api/v1/routes/calendar.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from schemas.calendar import DailyScheduleResponse
 from crud.student_activity import get_daily_activity
-from crud.student_class import get_daily_classes
+from crud.student_class import get_daily_classes, get_monthly_classes
 from core.security import get_current_active_user
 from schemas.auth import User
 
@@ -41,3 +41,27 @@ async def get_daily_schedule(current_user: User = Depends(get_current_active_use
     return DailyScheduleResponse(
         date=today, classes=classes_list_dicts, activities=activities_list
     )
+
+@calendar_router.get("/monthly")
+async def get_monthly_schedule(current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "student":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only students can access their monthly schedule",
+        )
+    
+    today = date.today() 
+    
+    try:
+        monthly_schedule = await get_monthly_classes(current_user, today) 
+    except HTTPException:
+        raise
+    except Exception as e:
+        # Catch and handle other unexpected errors
+        print(f"Unexpected error in get_monthly_schedule: {e}") 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error fetching monthly activities.",
+        )
+    
+    return {"schedule": monthly_schedule}
